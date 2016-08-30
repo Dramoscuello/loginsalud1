a2fdded823517949d61501e34e1a9e53668d9576
Deimer Ramos Cuello <dramos.cuello@gmail.com>
2016-08-30 13:10:34 -0500

    commit inicial

---
+++ LoginSalud/Master.Master.vb
@@ -0,0 +1,18 @@
+﻿Public Class Master
+    Inherits System.Web.UI.MasterPage
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Label1.Text = Session("usuario")
+        If Session("tusuario") = 2 Then
+            hide1.Visible = False
+            hide2.Visible = False
+            hide3.Visible = False
+
+        ElseIf Session("tusuario") = 1 Then
+            hide1.Visible = True
+            hide2.Visible = True
+            hide3.Visible = True
+        End If
+    End Sub
+
+End Class
